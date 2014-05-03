a92834c4ad9073a696439f75546148e264841232
ivanenev <ivanenev@gmail.com>
2014-05-03 19:44:34 +0100

    files uploaded

---
+++ reflow_oven.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _reflow_oven_H_
+#define _reflow_oven_H_
+#include "Arduino.h"
+//add your includes for the project reflow_oven here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project reflow_oven here
+
+
+
+
+//Do not add code below this line
+#endif /* _reflow_oven_H_ */
